b6314751191b877ef83cdb8db75471d0bec5318a
Md Imam Ahasan <emamahasane@gmail.com>
2021-09-30 19:58:41 +0600

    Add files via upload

--- rw_visual.py
+++ rw_visual.py
@@ -4,3 +4,10 @@ from random_walk import RandomWalk
 
 # Make a random walk.
 rw = RandomWalk()
+rw.fill_walk()
+
+# Plot the points in the walk
+plt.style.use("classic")
+fig, ax = plt.subplots()
+ax.scatter(rw.x_values, rw.y_values, s = 15)
+plt.show()
